36f3fb78ae0736d74147c0711cae94615b057487
Andrew Jeffery <dev@jeffas.io>
2023-06-15 21:31:08 +0100

    Update flake

--- flake.nix
+++ flake.nix
@@ -6,28 +6,28 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
     flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs { system = system; };
-          jekyll_env = pkgs.bundlerEnv {
-            name = "blog";
-            ruby = pkgs.ruby;
-            gemfile = ./Gemfile;
-            lockfile = ./Gemfile.lock;
-            gemset = ./gemset.nix;
-          };
-        in
-        {
-          devShell = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              jekyll_env
-
-              rnix-lsp
-              nixpkgs-fmt
-            ];
-          };
-        }
-      );
+    (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+        jekyll_env = pkgs.bundlerEnv {
+          name = "blog";
+          ruby = pkgs.ruby;
+          gemfile = ./Gemfile;
+          lockfile = ./Gemfile.lock;
+          gemset = ./gemset.nix;
+        };
+      in {
+        devShell = pkgs.mkShell {
+          packages = [
+            jekyll_env
+          ];
+        };
+      }
+    );
 }
